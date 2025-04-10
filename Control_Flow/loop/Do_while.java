53ad12e99827215b79f529331449bbf43331fd45
shambel <shambel5110@gmail.com>
2025-04-10 10:36:57 +0300

    Update Do_while.java

--- Control_Flow/loop/Do_while.java
+++ Control_Flow/loop/Do_while.java
@@ -1,7 +1,10 @@
-package Control_Flow.loop;
+import java.util.*;
+public class FOREACH{
 
-public class Do_while {
     public static void main(String[] args) {
-        
+        System.out.println("HI HELLO WORLD");
+        Scanner d=new Scanner(System.in);
+        int num=d.nextInt();
+        System.out.println(num);
     }
 }
